ba141ee131860d308dd5bb46447e8113c9b6e570
Julián Sánchez <50778235+juliandsg0222@users.noreply.github.com>
2024-05-20 09:50:35 -0500

    Update with latest config

--- src/App.js
+++ src/App.js
@@ -3,27 +3,22 @@ import { useState } from "react";
 import EditorJS from "@editorjs/editorjs";
 import Header from "@editorjs/header";
 import List from "@editorjs/list";
-// import Undo from "editorjs-undo";
-import ToggleBlock from "editorjs-toggle-block";
+import Undo from "editorjs-undo";
 
 function App() {
 	// eslint-disable-next-line no-unused-vars
 	const [editor, _setEditor] = useState(
 		() =>
 			new EditorJS({
-				// onReady: () => {
-				// 	new Undo({ editor });
-				// },
+				onReady: () => {
+					new Undo({ editor });
+				},
 				autofocus: true,
 				holder: "editorjs",
 				readOnly: false,
 				tools: {
 					header: Header,
 					list: List,
-					toggle: {
-						class: ToggleBlock,
-						inlineToolbar: true,
-					},
 				},
 			})
 	);
