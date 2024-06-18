7e53fcf633151583390d1a714ccc188fe37e2de8
Julián Sánchez <50778235+juliandsg0222@users.noreply.github.com>
2024-06-18 17:25:44 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,81 @@
-import "./App.css";
-import { useState } from "react";
-import EditorJS from "@editorjs/editorjs";
-import Header from "@editorjs/header";
-import List from "@editorjs/list";
-import Undo from "editorjs-undo";
+import './App.css';
+import { useState } from 'react';
+import EditorJS from '@editorjs/editorjs';
+import Paragraph from '@editorjs/paragraph';
+import Header from '@editorjs/header';
+import List from '@editorjs/list';
+import Undo from 'editorjs-undo';
+import DragDrop from 'editorjs-drag-drop';
+import ImageTool from '@editorjs/image';
+import InlineImage from 'editorjs-inline-image';
 
 function App() {
-	// eslint-disable-next-line no-unused-vars
-	const [editor, _setEditor] = useState(
+	const initialData = {
+		blocks: [
+			{
+				id: 'mIz7N25ijFi',
+				type: 'paragraph',
+				data: {
+					text: 'Hello',
+				},
+			},
+			{
+				id: 'mIz7825ijF',
+				type: 'paragraph',
+				data: {
+					text: 'world',
+				},
+			},
+			{
+				id: '5b7MCJYRaP',
+				type: 'image',
+				data: {
+					url: 'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
+					caption: '',
+					withBorder: false,
+					withBackground: false,
+					stretched: false,
+				},
+			},
+		],
+	};
+
+	const [editor] = useState(
 		() =>
 			new EditorJS({
-				onReady: () => {
-					new Undo({ editor });
-				},
 				autofocus: true,
-				holder: "editorjs",
+				holder: 'editorjs',
 				readOnly: false,
 				tools: {
 					header: Header,
 					list: List,
+					image: {
+						class: InlineImage,
+						inlineToolbar: true,
+						config: {
+							embed: {
+								display: true,
+							},
+						},
+					},
+					paragraph: {
+						class: Paragraph,
+						inlineToolbar: true,
+						config: { preserveBlank: true },
+					},
 				},
+				onReady: () => {
+					const undo = new Undo({ editor });
+					undo.initialize(initialData);
+					// new DragDrop(editor)
+				},
+				data: initialData,
 			})
 	);
 
 	const save = () => {
 		editor.save().then((savedData) => {
-			const output = document.getElementById("output");
+			const output = document.getElementById('output');
 
 			output.innerHTML = JSON.stringify(savedData, null, 4);
 		});
